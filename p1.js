a730be0bbc00506c761cf6f2906204975f3b8d02
Dani G <atencion@danigil.com>
2020-01-06 21:28:18 +0000

    'preg_dat_as' ('as' abrevia analizador sintáctico ) ahora lee el json de todos las preguntas, no solo la primera, para que pueda ahora calcular la pregunta de la peor marca

--- p1.js
+++ p1.js
@@ -13,7 +13,8 @@ entrada0.onchange = function() {
 function leer_a_preg( preg_v, archivo_x ) {
 	lector = new FileReader();
 	lector.onloadend = function() {
-		preg_dat = JSON.parse( lector.result );
+		preg_v = [];
+		preg_dat_as( preg_v, JSON.parse( lector.result ) );
 		/* hacemos una prueba con la pregunta preg_dat[0]
 		   el formato de preg_datos[ii] se convierte en
 		   preg[ii]
@@ -29,13 +30,11 @@ function leer_a_preg( preg_v, archivo_x ) {
 		 preg[ii].marca es la marca, igual como en
 		   "preg_dat"
 		 */
-		preg[0] = {};
-		preg_dat_as( preg_dat[0], preg[0] );
-		document.body.innerHTML = preg[0].html;
+		document.body.innerHTML = preg_v[0].html;
 		document.body.innerHTML +=
 		'<input type="button" value="seguir">';
 		document.querySelector("input[type='button']").
-			onclick = al_rsp_rll ( preg[0] );
+			onclick = al_rsp_rll ( preg_v[0] );
 	}
 	if( archivo_x ) lector.readAsText( archivo_x );
 }
@@ -63,39 +62,43 @@ function marca( rsp, slcn, texto ) {
 			marca_x++;
 	return marca_x / slcn.length;
 }
-function preg_dat_as( preg_dat_x, preg_x) {
-	preg_x.html = "", xx = 0;
-	preg_x.slcn = new Array;
-	preg_x.marca = [], preg_x.preg = [];
-	ahtml = {"&": "&amp;",
-			 "<": "&lt;",
-			 ">": "&gt;",
-			 "\"": "&quot;"
-			};
-	for( ii of preg_dat_x.ctndo ) {
-		if( !Array.isArray( ii ) ) {
-			preg_x.preg.push( ii );
-			preg_x.html += ii.replace(
-				/>|<|"|&/g,
-				function(xx) { return ahtml[ xx ]; }
-			);
-		}
-		/* al llegar a un arreglo de soluciones mientras
-		   itera por preg_dat_x, se añade un cuadrado de
-		   texto para cada respuesta, esto a preg_x.html,
-		   y el arreglo se añade sin las soluciones a
-		   la plantilla de la pregunta, preg_x.preg
-		 */
-		else {
-			aa = [];
-			for( jj of ii ) {
-				aa.push(xx);
-				preg_x.slcn[xx] = jj;
-				preg_x.html +=  `<input id = "e${xx++}">`;
+function preg_dat_as( preg_v, dat_x ) {
+	for( preg_dat_x of dat_x ) {
+		preg_x = {};
+		preg_x.html = "", xx = 0;
+		preg_x.slcn = new Array;
+		preg_x.marca = [], preg_x.preg = [];
+		ahtml = {"&": "&amp;",
+				 "<": "&lt;",
+				 ">": "&gt;",
+				 "\"": "&quot;"
+				};
+		for( ii of preg_dat_x.ctndo ) {
+			if( !Array.isArray( ii ) ) {
+				preg_x.preg.push( ii );
+				preg_x.html += ii.replace(
+					/>|<|"|&/g,
+					function(xx) { return ahtml[ xx ]; }
+				);
 			}
-			preg_x.preg.push( aa );
-		}
+			/* al llegar a un arreglo de soluciones mientras
+			   itera por preg_dat_x, se añade un cuadrado de
+			   texto para cada respuesta, esto a preg_x.html,
+			   y el arreglo se añade sin las soluciones a
+			   la plantilla de la pregunta, preg_x.preg
+			 */
+			else {
+				aa = [];
+				for( jj of ii ) {
+					aa.push(xx);
+					preg_x.slcn[xx] = jj;
+					preg_x.html +=  `<input id = "e${xx++}">`;
+				}
+				preg_x.preg.push( aa );
+			}
+		}	// fin del bucle por datos de preg. ii
+		preg_x.marca = preg_dat_x.marca;
+		preg_v.push( preg_x );
 	}
-	preg_x.marca = preg_dat_x.marca;
 	return 0;
 }
